eec4aabc733b7df1d771c4e847b13701eb1a32e6
polamjag <s@polamjag.info>
2015-02-14 07:32:15 +0900

    # Dotenv.load

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv.load
+#Dotenv.load
 Mongoid.raise_not_found_error = false
 
 module Traxlist2
