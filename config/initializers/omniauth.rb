3fdfe3d04fa9c51a61d4c332b1ddcdeb0b86ed15
polamjag <s@polamjag.info>
2015-06-06 20:45:55 +0900

    urgent fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
+OmniAuth.config.full_host = "http://traxlist.polamjag.info" if Rails.env.production?
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :twitter, ENV["OMNIAUTH_TWITTER_CK"], ENV["OMNIAUTH_TWITTER_CS"]
-  provider :twitter, "uQ6aYq98FwXeipQL7JKnk11fC", "hOFPUEy9NWwFqeIa3ulGZMs5lObZ1J6JrbD5IGEGYat4FMiS6R"
+  provider :twitter, ENV["OMNIAUTH_TWITTER_CK"], ENV["OMNIAUTH_TWITTER_CS"]
 end
